e45647a5ac86a49a3c1a4809c876efee0079ef57
andy-verbist <andy-verbist@users.noreply.github.com>
2014-02-05 22:30:29 +0100

    Wijziging User-class

--- src/main/Source Files/be/kdg/model/User.java
+++ src/main/Source Files/be/kdg/model/User.java
@@ -6,17 +6,57 @@
 package be.kdg.model;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "T_USER")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     private String username;
     private String password;
     private String eMail;
 
+    public User() {
+    }
+
     public User(String username, String password, String eMail) {
         this.username = username;
         this.password = password;
+        this.eMail = eMail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
         this.eMail = eMail;
     }
 }
