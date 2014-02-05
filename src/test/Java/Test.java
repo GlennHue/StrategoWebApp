c0f754c8e16ef8f23e7a4e72676caeab6f54d334
GlennHue <glenn.vanlooveren@student.kdg.be>
2014-02-05 10:17:58 +0100

    Initial commit

---
+++ src/test/Java/Test.java
@@ -0,0 +1,12 @@
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Glenn on 5/02/14.
+ */
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        assertTrue("derp", false);
+    }
+}
