645fdc2503cb1fc33b35120226459d766b69b72e
GlennHue <glenn.vanlooveren@student.kdg.be>
2014-02-05 11:15:20 +0100

    Setting up database

--- src/main/Source Files/be/kdg/model/User.java
+++ src/main/Source Files/be/kdg/model/User.java
@@ -6,7 +6,17 @@
 package be.kdg.model;
 
 
+import javax.persistence.Entity;
 
+@Entity
 public class User {
-    private int i = 0;
+    private String username;
+    private String password;
+    private String eMail;
+
+    public User(String username, String password, String eMail) {
+        this.username = username;
+        this.password = password;
+        this.eMail = eMail;
+    }
 }
--- src/test/Java/Test.java
+++ src/test/Java/Test.java
@@ -4,7 +4,13 @@
 * 2013-2014
 */
 
-import static junit.framework.Assert.assertTrue;
+import be.kdg.model.User;
+import be.kdg.persistence.HibernateUtil;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import static junit.framework.Assert.assertEquals;
 
 
 public class Test {
@@ -12,6 +18,17 @@ public class Test {
     @org.junit.Test
     public void test() {
         User user = new User("username", "password", "email");
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
 
+        session.saveOrUpdate(user);
+        tx.commit();
+
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+        tx = session.beginTransaction();
+
+        Query query = session.createQuery("from be.kdg.model.User");
+        User testUser = (User)query.uniqueResult();
+        assertEquals("users should be the same", user, testUser);
     }
 }
