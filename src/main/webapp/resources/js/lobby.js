3df204da14e5b1ed4b5f2cf0a92f055d4f54ea19
andy-verbist <andy-verbist@users.noreply.github.com>
2014-03-06 17:48:11 +0100

    lobby

--- src/main/webapp/resources/js/lobby.js
+++ src/main/webapp/resources/js/lobby.js
@@ -1,12 +1,13 @@
-	
+function queue(){
 
-    function queue(){
-     
-        var element = document.getElementById("queueFrm:text");
-        var jBtn = document.getElementById("joinQueue:joinBtn");
-     
-        jBtn.click();
-     
-        element.innerHTML=response.name;
-    }
+    var element = document.getElementById("queueFrm:text");
+    var jBtn = document.getElementById("joinQueue:joinBtn");
 
+    jBtn.click();
+
+    element.innerHTML=response.name;
+}
+
+
+
+   
