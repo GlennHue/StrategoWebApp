710902812ef193ea9e5f7a85c91d092a91b583c5
andy-verbist <andy.verbist@student.kdg.be>
2014-03-06 17:42:42 +0100

    Create lobby.js

---
+++ src/main/webapp/resources/js/lobby.js
@@ -0,0 +1,12 @@
+	
+
+    function queue(){
+     
+        var element = document.getElementById("queueFrm:text");
+        var jBtn = document.getElementById("joinQueue:joinBtn");
+     
+        jBtn.click();
+     
+        element.innerHTML=response.name;
+    }
+
