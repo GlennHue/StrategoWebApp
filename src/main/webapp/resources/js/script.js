e965f798934eac9f40b61b826c9147eac0a5212b
Thomas <thomas.vromans@student.kdg.be>
2014-03-23 20:50:02 +0100

    Achievements pagina toont nu ook de game history.

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -7,3 +7,32 @@ $(document).ready(function () {
         $("#loginForm").hide();
     })
 });
+
+function getPastGames() {
+    var player = $("#un").text();
+
+    $.getJSON("http://localhost:8080/api/user/getGameHistory?username=" + player)
+        .done(function(data) {
+            var g = data.games;
+            var arrayLength = g.length;
+            var rows ="";
+
+            for (var i = 0; i < arrayLength; i++) {
+             //   alert(g[i].gameId);
+                var p = g[i].players;
+                var aL = p.length;
+                 rows += "<tr>";
+                for (var i = 0; i < aL; i++) {
+                     var s = p[i].username;
+                     var score = p[i].score;
+
+                    rows += "<td>" + s + "</td><td>" + score + "</td>";
+                }
+                rows += "</tr>";
+            }
+            $( rows ).appendTo( "#historyList tbody" );
+        })
+        .fail(function() {
+            alert("get turn fail " + player);
+        });
+}
